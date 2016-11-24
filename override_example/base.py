3282de554ffb1b834c621fab51c68cd16a06802b
NumbShell <acxey32@gmail.com>
2016-11-24 10:14:27 +0100

    Added comments

--- override_example/base.py
+++ override_example/base.py
@@ -1,7 +1,8 @@
 
-
+#Create Class Base that inherent from object (a base class stored in python that allow inheritance to happen)
 class Base(object):
 
+    #init a variable
     def __init__(self):
         self.x = 5
 
--- override_example/classes.py
+++ override_example/classes.py
@@ -1,23 +1,31 @@
 from override_example import base as b
 
-
+#Create a Class that inherent from Base
 class inheriting(b.Base):
 
+    #Run init to define variables
     def __init__(self):
+        #Init the Base Class
+        super().__init__()
         self.x = 17
 
 
 class inheritingTwo(b.Base):
 
+    #Override the printMe method from Base class
     def printMe(self):
-        print ("You just override Welcome from the Base class!")
+        print ("You just override the printMe method in Base class!")
 
 
+#Create some objects
 obj = inheritingTwo()
 obj1 = inheriting()
 obj2 = b.Base()
 
+#Prints out the subclasses of class Base
+print(b.Base.__subclasses__())
 
+#Prints out some stuff
 obj.printMe()
 print(obj1.x)
 
